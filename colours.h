5b553d3278bc38a590140d0c6015c5a848a30a62
hdgarrood <hdgarrood@gmail.com>
2012-02-05 21:32:23 +0000

    rearranging

---
+++ colours.h
@@ -0,0 +1,17 @@
+/*
+ * colours.h
+ *
+ *  Created on: Feb 5, 2012
+ *      Author: Harry
+ */
+
+#ifndef COLOURS_H_
+#define COLOURS_H_
+
+#include "SDL/SDL.h"
+
+SDL_Colour WHITE = {255,255,255};
+SDL_Colour BLACK = {0,0,0};
+
+
+#endif /* COLOURS_H_ */
